c7f46b4c519181ae4f0b1bb802d5787e54eb0fe1
haluc4280-maker <haluc4280@gmail.com>
2026-09-24 13:57:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ CSV_FILE = "history.csv"
 menu = {
     "Đồ ăn": {
         "Pizza Hải Sản": 150000,
-        "Mì Ý Bò Bằm": 95000,
-        "Burger Gà": 65000,
+        "Mì Ý heo Bằm": 95000,
+        "Burger khủng long": 65000,
         "Salad Trộn": 50000,
-        "Bít tết Bò Mỹ": 250000,
+        "Bít tết Bò lai mèo": 250000,
         "Sườn nướng BBQ": 180000,
-        "Cánh gà chiên mắm": 75000,
+        "Cánh chim chiên mắm": 75000,
         "Lẩu cá diêu hồng": 200000,
         "Lẩu Thái hải sản": 300000,
     },
